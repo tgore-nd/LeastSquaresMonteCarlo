f4ec514e20aa48a490a6c94f537dd0b1606b6b8c
Tom Gore <tgore@nd.edu>
2025-06-30 20:25:55 -0600

    Add LinearRegressor class

---
+++ math/regression.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class LinearRegressor:
+    def __init__(self):
+        self.parameters = None
+    
+    def fit(self, X: np.array, y: np.array) -> None:
+        """Fit the LinearRegressor to some data."""
+        # Add intercept term: X = [1, x1, ...]
+        X = np.hstack(
+            [np.ones((X.shape[0], 1)), X]
+        )
+
+        # Compute parameters via normal equation
+        self.parameters = np.linalg.inv(X.T @ X) @ X.T @ y
+    
+    def predict(self, X: np.array) -> np.array:
+        """Given a feature matrix X, predict the value of the response vector y."""
+        X = np.hstack(
+            [np.ones((X.shape[0], 1)), X]
+        )
+        return X @ self.parameters
--- simulations/heston.py
+++ simulations/heston.py
@@ -1,21 +1,21 @@
 import numpy as np
 
-def generate_heston_paths(tau, kappa, theta, sigma, rho, v0, S0, r, N, M):
+def generate_heston_paths(tau, kappa, theta, sigma, rho, v0, S0: float, r: float, N, M) -> tuple[np.array, np.array]:
     """
     Inputs:
-     - tau   : time of simulation
-     - kappa : rate of mean reversion in variance process
-     - theta : long-term mean of variance process
-     - sigma : vol of vol / volatility of variance process
-     - rho   : correlation between asset returns and variance
-     - S0, v0: initial parameters for asset and variance
-     - r     : interest rate
-     - N     : number of time steps
-     - M     : number of asset paths
+     - tau    : time of simulation
+     - kappa  : rate of mean reversion in variance process
+     - theta  : long-term mean of variance process
+     - sigma  : vol of vol / volatility of variance process
+     - rho    : correlation between asset returns and variance
+     - S0, v0 : initial parameters for asset and variance
+     - r      : interest rate
+     - N      : number of time steps
+     - M      : number of asset paths
     
     Outputs:
-    - asset prices over time (numpy array)
-    - variance over time (numpy array)
+    - asset prices over time
+    - variance over time
     """
     # initialise other parameters
     dt = tau/N
@@ -34,3 +34,15 @@ def generate_heston_paths(tau, kappa, theta, sigma, rho, v0, S0, r, N, M):
         v[i] = np.maximum(v[i - 1] + kappa*(theta - v[i - 1]) * dt + sigma * np.sqrt(v[i - 1] * dt) * Z[i - 1, :, 1], 0)
     
     return S, v
+
+if __name__ == "__main__":
+    kappa = 2.0
+    theta = 0.04
+    sigma = 0.3
+    rho = -0.7
+    v0 = 0.04
+    r = 0.01
+    S0 = 100.
+    tau = 1.0
+    # Test getting price matrix
+    print(generate_heston_paths(tau, kappa, theta, sigma, rho, v0, S0, r, 100, 10)[0])
