25fe5d8ea59c7f59f11c6c6cafe342984c3c775f
Tom Gore <tgore@nd.edu>
2025-07-26 19:03:06 -0600

    Fix column appending method

--- math_apps/basis_expansions.py
+++ math_apps/basis_expansions.py
@@ -1,57 +1,67 @@
 import numpy as np
 from typing import Literal
 
-# Legendre, Hermite, Spline
+# Legendre, Hermite, Spline, Chebyshev, and Jacobi
 
 
-def polynomial_basis(X: np.ndarray, N: int, i: int = 0) -> np.ndarray:
-    """Modify the ith column of the input array for an Nth-degree basis expansion."""
-    return np.concatenate([X] + [X[:, i] ** n for n in range(2, N + 1)], axis = 1)
+def polynomial_basis(X: np.ndarray, N: int) -> np.ndarray:
+    """Take an Nth-degree polynomial basis expansion of vector X."""
+    return np.column_stack([X, *[X ** n for n in range(2, N + 1)]])
 
 
 def laguerre_basis(X: np.ndarray, N: int, i: int = 0) -> np.ndarray:
+    """Take an Nth-degree Laguerre basis expansion of vector X. The original feature is removed!"""
     L = np.zeros((X.shape[0], N))
     L[:, 0] = 1.0 # L0 = 1
 
     # Ln
     if N > 1:
-        L[:, 1] = 1.0 - X[:, i]
+        L[:, 1] = 1.0 - X
         for n in range(1, N - 1):
             # Evaluate using recurrence relation
-            L[:, n + 1] = ((2 * n + 1 - X[:, i]) * L[:, n] - n * L[:, n - 1]) / (n + 1)
+            L[:, n + 1] = ((2 * n + 1 - X) * L[:, n] - n * L[:, n - 1]) / (n + 1)
     
-    return np.concatenate([X, L], axis = 1)
+    #return np.concatenate([X.T, L], axis = 0).T
+    return np.column_stack([X, *[L[:, i] for i in range(L.shape[1])]])[:, 1:]
 
 
 def legendre_basis(X: np.ndarray, N: int, i: int = 0) -> np.ndarray:
+    """Take an Nth-degree Legendre basis expansion of vector X. The original feature is removed!"""
     P = np.zeros((X.shape[0], N))
     P[:, 0] = 1.0 # P0 = 1
 
     # Pn
     if N > 1:
-        P[:, 1] = X[:, i]
+        P[:, 1] = X
         for n in range(1, N - 1):
             # Evaluate using recurrence relation
-            P[:, n + 1] = ((2 * n + 1) * X[:, i] * P[:, n] - n * P[:, n - 1]) / (n + 1)
+            P[:, n + 1] = ((2 * n + 1) * X * P[:, n] - n * P[:, n - 1]) / (n + 1)
     
-    return np.concatenate([X, P], axis = 1)
+    return np.column_stack([X, *[P[:, i] for i in range(P.shape[1])]])[:, 1:]
 
 
-def hermite_basis(X: np.ndarray, N: int, i: int = 0, type: Literal["probabilist", "physicist"] = "probabilist"):
+def hermite_basis(X: np.ndarray, N: int, i: int = 0, type: Literal["probabilist", "physicist"] = "probabilist") -> np.ndarray:
+    """Take an Nth-degree Hermite basis expansion of vector X. The original feature is removed!"""
     H = np.zeros((X.shape[0], N))
     H[:, 0] = 1.0
 
     if N > 1:
         if type == "probabilist":
-            H[:, 1] = X[:, i]
+            H[:, 1] = X
             for n in range(1, N - 1):
                 # Evaluate using recurrence relation
-                H[:, n + 1] = X[:, i] * H[:, n] - n * H[:, n - 1]
+                H[:, n + 1] = X * H[:, n] - n * H[:, n - 1]
         elif type == "physicist":
-            H[:, 1] = 2 * X[:, i]
+            H[:, 1] = 2 * X
             for n in range(1, N - 1):
-                H[:, n + 1] = 2 * X[:, i] * H[:, n] - 2 * n * H[:, n - 1]
+                H[:, n + 1] = 2 * X * H[:, n] - 2 * n * H[:, n - 1]
         else:
             raise ValueError("Argument 'type' must be either 'probabilist' or 'physicist'")
     
-    return np.concatenate([X, H], axis = 1)
+    return np.column_stack([X, *[H[:, i] for i in range(H.shape[1])]])[:, 1:]
+
+
+if __name__ == "__main__":
+    X = np.array([1.08, 1.07, 0.97, 0.77, 0.84])
+    X_exp = hermite_basis(X, 2)
+    print(X_exp)
